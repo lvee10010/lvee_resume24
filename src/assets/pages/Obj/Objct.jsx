d7d69e0f23adf6e50f02007a96f30b77d0250dfa
lvee10010 <levyjltb021@gmail.com>
2024-05-28 22:00:41 +0800

    Update Objct.jsx
    color change

--- src/assets/pages/Obj/Objct.jsx
+++ src/assets/pages/Obj/Objct.jsx
@@ -46,7 +46,7 @@ function Objct() {
     function fragmentShader() {
       return `
           void main() {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
           }
         `;
     }
